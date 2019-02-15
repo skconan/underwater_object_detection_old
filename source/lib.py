6b976ffde87410ce0661330dab71905040ae6380
skconan <supakit.kr@gmail.com>
2019-02-15 12:18:38 +0700

    add function

--- source/lib.py
+++ source/lib.py
@@ -9,4 +9,17 @@
 
 import constans as CONST
 import numpy as np
-import cv2 as cv2
+import cv2 as cv
+
+def get_kernel(shape='rect', ksize=(5, 5)):
+    if shape == 'rect':
+        return cv.getStructuringElement(cv.MORPH_RECT, ksize)
+    elif shape == 'ellipse':
+        return cv.getStructuringElement(cv.MORPH_ELLIPSE, ksize)
+    elif shape == 'plus':
+        return cv.getStructuringElement(cv.MORPH_CROSS, ksize)
+    else:
+        return None
+
+def normalize(gray):
+    return np.uint8(255 * (gray - gray.min()) / (gray.max() - gray.min()))
